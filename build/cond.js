cdd85af551717d52e75e44410688a83d4f7fbc8e
Kat Marchán <kzm@sykosomatic.org>
2014-03-16 22:46:09 -0700

    Updating build files

--- build/cond.js
+++ build/cond.js
@@ -57,32 +57,66 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	"use strict";
 	
+	function warn(cond) {
+	  arguments[0] = typeof cond === "string" ? new Warning(cond) : cond;
+	  return signal.apply(this, arguments);
+	}
+	
+	function Warning() {}
+	Warning.prototype = new Error;
+	Warning.prototype.constructor = Warning;
+	
+	function cerror() {
+	  return error.apply(this, [
+	    arguments[0],
+	    ["continue",
+	     "Return undefined (or a value) and continue normally",
+	     function(x){return x;}]
+	  ].concat([].slice.call(arguments, 1)));
+	}
+	
+	function error(cond) {
+	  arguments[0] = typeof cond === "string" ? new Error(cond) : cond;
+	  return signal.apply(this, arguments);
+	}
+	
 	function signal(cond) {
 	  if (arguments.length <= 1) {
 	    return _signal(cond);
 	  } else {
-	    return restartCase.apply(
+	    return recoverable.apply(
 	      this,
 	      [function(){return _signal(cond);}].concat([].slice.call(arguments, 1)));
 	  }
 	}
 	
 	function _signal(cond) {
-	  HANDLERS.forEach(function(handlerEntry) {
-	    if (cond instanceof handlerEntry[0]) {
-	      handlerEntry[1](cond);
-	    }
-	  });
+	  var oldClusters = HANDLER_CLUSTERS;
+	  try {
+	    HANDLER_CLUSTERS.forEach(function(cluster) {
+	      HANDLER_CLUSTERS = HANDLER_CLUSTERS.slice(1);
+	      cluster.forEach(function(handlerEntry) {
+	        if (cond instanceof handlerEntry[0]) {
+	          handlerEntry[1](cond);
+	        }
+	      });
+	    });
+	  } finally {
+	    HANDLER_CLUSTERS = oldClusters;
+	  }
 	}
 	
 	function handlerBind(handledBody) {
 	  var handlers = [].slice.call(arguments, 1),
-	      oldHandlers = HANDLERS;
+	      oldClusters = HANDLER_CLUSTERS;
 	  try {
-	    HANDLERS = handlers.concat(HANDLERS);
+	    HANDLER_CLUSTERS = [handlers].concat(HANDLER_CLUSTERS);
 	    return handledBody.call(this);
+	  } catch (e) {
+	    signal(e);
+	    throw e;
 	  } finally {
-	    HANDLERS = oldHandlers;
+	    HANDLER_CLUSTERS = oldClusters;
 	  }
 	}
 	
@@ -106,18 +140,18 @@ return /******/ (function(modules) { // webpackBootstrap
 	  }
 	}
 	
-	function listRestarts() {
-	  // "Deep enough" copy to protect the internal RESTARTS array structure from
+	function listRecoveries() {
+	  // "Deep enough" copy to protect the internal RECOVERIES array structure from
 	  // user shenanigans.
-	  return RESTARTS.map(function(x) {
+	  return RECOVERIES.map(function(x) {
 	    return x.map(function(x) { return x; });
 	  });
 	}
 	
-	function restartCase(restartableBody) {
+	function recoverable(recoverableBody) {
 	  var sentinel = {},
-	      oldRestarts = RESTARTS,
-	      restarts = [].slice.call(arguments, 1).map(function(entry) {
+	      oldRecoveries = RECOVERIES,
+	      recoveries = [].slice.call(arguments, 1).map(function(entry) {
 	        var name = entry[0],
 	            description = typeof entry[1] === "string" ? entry[1] : "",
 	            callback = description ? entry[2] : entry[1];
@@ -128,8 +162,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	        }];
 	      });
 	  try {
-	    RESTARTS = restarts.concat(RESTARTS);
-	    return restartableBody.call(this);
+	    RECOVERIES = recoveries.concat(RECOVERIES);
+	    return recoverableBody.call(this);
 	  } catch(e) {
 	    if (e === sentinel) {
 	      return sentinel.callback.apply(this, sentinel.args);
@@ -137,29 +171,39 @@ return /******/ (function(modules) { // webpackBootstrap
 	      throw e;
 	    }
 	  } finally {
-	    RESTARTS = oldRestarts;
+	    RECOVERIES = oldRecoveries;
 	  }
 	}
 	
-	function restart(name) {
-	  return _restart(name);
+	function recover(name) {
+	  return _recover.apply(this, arguments);
 	}
-	function _restart(name) {
-	  var restart = Array.isArray(name) ? name : findRestart(name),
-	      restartArgs = [].slice.call(arguments, 1);
-	  restart[restart.length-1].apply(this, restartArgs);
+	function _recover(name) {
+	  var recovery = Array.isArray(name) ? name : findRecovery(name),
+	      recoveryArgs = [].slice.call(arguments, 1),
+	      oldThis = this;
+	  if (recovery) {
+	    return recovery[recovery.length-1].apply(this, recoveryArgs);
+	  } else {
+	    return cerror("Recovery not found: "+name, [
+	      "try-again", "Call recover() again with a new name", function(x) {
+	        return recover.apply(oldThis, recoveryArgs);
+	      }
+	    ]);
+	
+	  }
 	}
 	
-	function findRestart(name) {
+	function findRecovery(name) {
 	  if (typeof name === "string") {
-	    for (var i = 0; i < RESTARTS.length; i++) {
-	      if (name === RESTARTS[i][0]) {
-	        return RESTARTS[i];
+	    for (var i = 0; i < RECOVERIES.length; i++) {
+	      if (name === RECOVERIES[i][0]) {
+	        return RECOVERIES[i];
 	      }
-	      return undefined;
 	    }
+	    return undefined;
 	  } else {
-	    return RESTARTS[name];
+	    return RECOVERIES[name];
 	  }
 	}
 	
@@ -170,46 +214,46 @@ return /******/ (function(modules) { // webpackBootstrap
 	  /*****************************************************************/
 	  /* Welcome to the */ debugger; /* Read below for instructions!!! */
 	  /*                                                               */
-	  /* Restarts may be available.                                    */
-	  /* Call showRestarts() in the JS console to list them.           */
+	  /* Recoveries may be available.                                    */
+	  /* Call showRecoveries() in the JS console to list them.           */
 	  /*                                                               */
-	  /* If you pick a restart, it will be invoked after you unpause   */
+	  /* If you pick a recovery, it will be invoked after you unpause   */
 	  /* the debugger. Otherwise, `condition` will be thrown.          */
 	  /*                                                               */
 	  /*                Thanks for using CondJS!                       */
 	  /*                                                               */
 	  /*****************************************************************/
-	  
-	  var __chosenRestart,
-	      __restartArgs;
-	  function restart(name) {
-	    __chosenRestart = name;
-	    __restartArgs = arguments;
-	    console.log("You have chosen restart: ", name);
+	
+	  var __chosenRecovery,
+	      __recoveryArgs;
+	  function recover(name) {
+	    __chosenRecovery = name;
+	    __recoveryArgs = arguments;
+	    console.log("You have chosen recovery: ", name);
 	    console.log("Unpause the debugger to continue.");
 	  }
-	  function showRestarts() {
+	  function showRecoveries() {
 	    console.log(
 	      (((condition && condition.toString) ? condition.toString() + "\n": "") +
-	       "Available restarts: \n" +
+	       "Available recoveries: \n" +
 	       "\n" +
-	       RESTARTS.reduce(function(acc, entry, i) {
-	         return acc + formatRestart(entry, i) + "\n";
+	       RECOVERIES.reduce(function(acc, entry, i) {
+	         return acc + formatRecovery(entry, i) + "\n";
 	       }, "") + "\n" +
-	       "To use a restart, use `restart(<name or index>[, arg1[, arg2 ...]])`\n"+
+	       "To use a recovery: `recover(<name or index>[, arg1[, arg2 ...]])`\n"+
 	       "\n" +
 	       "Unpause your debugger to continue." +
 	       ""));
 	  }
 	
-	  if (__chosenRestart != null) {
-	    _restart.apply(this, __restartArgs);
+	  if (__chosenRecovery != null) {
+	    _recover.apply(this, __recoveryArgs);
 	  } else {
 	    throw condition;
 	  }
 	}
 	
-	function formatRestart(entry, i) {
+	function formatRecovery(entry, i) {
 	  var name = entry[0],
 	      description = (typeof entry[1] === "string" && entry[1].length) ?
 	        ": "+entry[1] :
@@ -221,20 +265,25 @@ return /******/ (function(modules) { // webpackBootstrap
 	 * Internals
 	 */
 	
-	var HANDLERS = [[
+	var HANDLER_CLUSTERS = [[
+	  [Warning, function(w) { console.warn(w); }],
 	  // If we get an error, force falling back into the debugger.
-	  Error, debug
+	  [Error, debug]
 	]],
-	    RESTARTS = [];
+	    RECOVERIES = [];
 	
 	
 	module.exports = {
 	  signal: signal,
+	  error: error,
+	  cerror: cerror,
+	  warn: warn,
+	  Warning: Warning,
 	  handlerBind: handlerBind,
 	  handlerCase: handlerCase,
-	  restartCase: restartCase,
-	  restart: restart,
-	  findRestart: findRestart,
+	  recoverable: recoverable,
+	  recover: recover,
+	  findRecovery: findRecovery,
 	  debug: debug
 	};
 
